29be196d1a5bbd7635d56e89cd367c155e0ebf22
kirillrezontov <163021417+kirillrezontov@users.noreply.github.com>
2026-03-23 01:19:27 +0500

    List.h

--- Containers-Group-Project/List.h
+++ Containers-Group-Project/List.h
@@ -1,16 +1,39 @@
 #pragma once
 #include "ListAbstract.h"
-#include "GroupContainer.h"
+
 class List : public AbstractList
 {
-	friend class GroupContainer;
-	struct Node {
+protected:
+	typedef struct Node
+	{
 		void* data;
 		size_t size;
 		Node* next;
-	};
-	size_t _size;
+	} Node;
+	int _size;
 	Node* _head;
-	class Iterator : public AbstractList::Iterator {
+	class Iterator : public Container::Iterator {
+	public:
+		Node* _this;
+		Node* _prev;
+		void* getElement(size_t& size) override;
+		bool hasNext() override;
+		void goToNext() override;
+		bool equals(Container::Iterator* right) override;
+	};
+public:
+	List(MemoryManager& mem) : AbstractList(mem), _head(nullptr), _size(0){}
+	int size() override;
+	size_t max_bytes() override;
+	Container::Iterator* find(void* elem, size_t size) override;
+	Container::Iterator* newIterator() override;
+	void remove(Container::Iterator* iter) override;
+	void clear() override;
+	bool empty() override;
 
-}
+	~List();
+	int push_front(void* elem, size_t elemSize) override;
+	void pop_front() override;
+	void* front(size_t& size) override;
+	int insert(Container::Iterator* iter, void* elem, size_t elemSize) override;
+};
